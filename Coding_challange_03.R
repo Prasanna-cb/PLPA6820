c7321702bc4e262bb40a3a7e5980d499abe48809
Prasanna-cb <ykd0002@auburn.edu>
2026-02-12 14:27:22 -0600

    commit2

---
+++ Coding_challange_03.R
@@ -0,0 +1,7 @@
+  ### coding challange 3 ###
+
+library("tidyverse")
+library(ggpubr)
+library(ggrepel)
+MycotoxinData=read.csv ("coding challnages data/MycotoxinData.csv", na.strings = "na") #file path to GitHub repository
+head(MycotoxinData) 
